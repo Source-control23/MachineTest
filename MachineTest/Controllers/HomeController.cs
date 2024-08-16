d11605333ef9321c1ba4fd92353e564397286178
sourcecontrol <sourcecontrol@cdpindia.com>
2024-08-16 11:05:22 +0530

    Add project files.

---
+++ MachineTest/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+using MachineTest.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace MachineTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var cartItems = new List<Items>
+            {
+                new Items { ItemName = "T-shirt", ItemPrice = 2000, Quantity = 6 },
+                new Items { ItemName = "Jeans", ItemPrice = 4000, Quantity = 8 },
+                new Items { ItemName = "Shoes", ItemPrice = 6000, Quantity = 4 },
+                new Items { ItemName = "Cap", ItemPrice = 2000, Quantity = 20 }
+            };
+
+            var total = 0;
+
+            ViewData["CartItems"] = cartItems;
+            ViewData["Total"] = total;
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        
+    }
+}
---
+++ MachineTest/Models/Items.cs
@@ -0,0 +1,9 @@
+﻿namespace MachineTest.Models
+{
+    public class Items
+    {
+        public int Quantity { get; set; }
+        public string ItemName { get; set; }
+        public float ItemPrice { get; set; }
+    }
+}
